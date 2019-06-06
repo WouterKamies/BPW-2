63eb9febc5b783a3e14765ebdddef8d4a543a89e
Weltor <kamies132779@gmail.com>
2019-06-06 21:13:18 +0200

    Working gravity and added first environment assets

---
+++ BPW 2/Assets/Code/Gravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float gravity;
+
+    public void OnTriggerStay(Collider other)
+    {
+    
+        if (other.gameObject.tag == "Gravity")
+        {
+
+            Vector3 toCenter = other.transform.position - transform.position;
+            toCenter.Normalize();
+
+            rb.AddForce(toCenter * gravity, ForceMode.Acceleration);
+
+            Quaternion quaternion = Quaternion.FromToRotation(transform.up, -toCenter);
+
+            quaternion = quaternion * transform.rotation;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, 1);
+
+        }
+
+    }
+
+}
